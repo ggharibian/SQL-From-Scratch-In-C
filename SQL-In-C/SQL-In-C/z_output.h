6fd7ec994be641356432bb02f0ee0ac981ce50f0
Garni Gharibian <gharibian.garni0@gmail.com>
2021-10-12 03:18:44 -0700

    Adding files for running main.

---
+++ SQL-In-C/SQL-In-C/z_output.h
@@ -0,0 +1,25 @@
+#ifndef Z_OUTPUT_H
+#define Z_OUTPUT_H
+
+
+/*
+***** ALL RESULTS ARE VERIFIED ******
+
+Bare Minimum Run:
+
+    [ unknown, from ]
+    [ table, student ]
+[ table, ]
+    [ field, last ]
+[ field, last ]
+    [ command, select ]
+
+
+C:\Users\gghar\OneDrive\Desktop\C++ Directory\CS008 Barkeshli\Final Project\Debug\Final Project.exe (process 20232) exited with code 0.
+To automatically close the console when debugging stops, enable Tools->Options->Debugging->Automatically close the console when debugging stops.
+Press any key to close this window . . .
+
+
+
+*/
+#endif // !Z_OUTPUT_H
---
+++ SQL-In-C/SQL-In-C/z_work_report.h
@@ -0,0 +1,32 @@
+#ifndef Z_WORK_REPORT_H
+#define Z_WORK_REPORT_H
+/*
+
+Features:
+        -Not Implemented:
+                None.
+
+        -Implemented:
+                Parser Class with the bare minimum commands and 
+                functionalities.
+
+        -Partly implemented:
+                Table Class is almost fully implemented, only missing the 
+                output operator due to a bug that will be fixed soon! 
+                Otherwise, the table class is also complete.
+
+    Bugs:
+            None with the lab at it's current state. The only bug that hasn't
+            been show is the aforementioned bug concerning the Table class.
+
+    Reflections:
+            This lab wasn't very difficult overall; with enough time and 
+            effort, the product will be complete. The only issue I ran 
+            into was concerning my Table class, which I recently upheaved 
+            in order to make it functional to your standards professor ;). 
+            Alternatively, the parser class was a piece of cake. No problems 
+            with that :))).
+
+
+*/
+#endif // !Z_WORK_REPORT_H
